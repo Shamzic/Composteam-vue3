ec186eddb4d85ec56790f28f41f73f4fb760e7ea
Shamzic <shamery@free.fr>
2022-08-06 00:52:44 +0200

    first commit

---
+++ src/pages/login.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+const { t } = useI18n()
+const router = useRouter()
+
+const go = () => {
+    console.log('go')
+}
+</script>
+
+<template>
+    <div>Login
+      <button
+        btn m-3 text-sm
+        @click="go"
+      >
+      TEST
+      </button>
+
+
+    </div>
+</template>
